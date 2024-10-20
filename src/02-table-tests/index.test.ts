3a282d389dd8aa55c0b64a887e133b098631fa31
Sergey-Ado <adonisiy2@gmail.com>
2024-10-20 16:37:57 +0400

    feat: update 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -3,26 +3,12 @@ import { simpleCalculator, Action } from './index';
 
 const testCases = [
   { a: 1, b: 2, action: Action.Add, expected: 3 },
-  { a: 2, b: 2, action: Action.Add, expected: 4 },
-  { a: 3, b: 2, action: Action.Add, expected: 5 },
-  { a: 1, b: 2, action: Action.Subtract, expected: -1 },
-  { a: 2, b: 2, action: Action.Subtract, expected: 0 },
-  { a: 3, b: 2, action: Action.Subtract, expected: 1 },
-  { a: 1, b: 2, action: Action.Multiply, expected: 2 },
-  { a: 2, b: 2, action: Action.Multiply, expected: 4 },
-  { a: 3, b: 2, action: Action.Multiply, expected: 6 },
-  { a: 1, b: 2, action: Action.Divide, expected: 0.5 },
-  { a: 2, b: 2, action: Action.Divide, expected: 1 },
-  { a: 3, b: 2, action: Action.Divide, expected: 1.5 },
-  { a: 1, b: 2, action: Action.Exponentiate, expected: 1 },
-  { a: 2, b: 2, action: Action.Exponentiate, expected: 4 },
-  { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
-  { a: 1, b: 2, action: 'invalidAction', expected: null },
-  { a: 2, b: 2, action: '%', expected: null },
-  { a: 3, b: 2, action: '++', expected: null },
-  { a: '1', b: 2, action: Action.Add, expected: null },
-  { a: false, b: true, action: Action.Add, expected: null },
-  { a: undefined, b: null, action: Action.Add, expected: null },
+  { a: 4, b: 1, action: Action.Subtract, expected: 3 },
+  { a: 2, b: 3, action: Action.Multiply, expected: 6 },
+  { a: 10, b: 2, action: Action.Divide, expected: 5 },
+  { a: 2, b: 3, action: Action.Exponentiate, expected: 8 },
+  { a: 2, b: 4, action: 'invalidAction', expected: null },
+  { a: '3', b: '5', action: Action.Add, expected: null },
 ];
 
 describe('simpleCalculator', () => {
